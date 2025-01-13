46b63faafafc0c576064d28097c594ecfd636252
Sandeep Kumar <Ksundip750@gmail.com>
2025-01-13 23:07:17 +0530

    add Navbar component and update app title; include Boxicons stylesheet and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../src/Components/UI/Style.css'
+import Navbar from './Components/Contents/Navbar';
 
 const App = () => {
   return (
     <div>
-      app
+      <Navbar/>
     </div>
   )
 }
---
+++ src/Components/Contents/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="nav">
+        <h4 className="logo">
+            NetFortress.Co
+        </h4>
+        <ul className="nav-list">
+            li.nav-item
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
